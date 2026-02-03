683f4e294f3f677bd5c293436e5369523e7241f3
BTHS <sample@bths.edu>
2026-02-03 10:56:28 -0500

    Box + extra helpers

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Student student1 = new Student("George", 4, 280);
-        System.out.println(student1);
+        Box b1 = new Box(10);
+        Box b2 = new Box(10, 5, 20);
+
+        System.out.println(b1);
+        System.out.println();
+        System.out.println(b2);
     }
 }
