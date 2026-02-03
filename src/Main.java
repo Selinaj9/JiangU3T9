8a9053dc466432604458d589a2dbb449a88ec683
BTHS <sample@bths.edu>
2026-02-03 10:45:28 -0500

    Initial commit/Student

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student("George", 4, 280);
+        System.out.println(student1);
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    private String name;
+    private int tests;
+    private int points;
+
+    public Student(String name, int tests, int points) {
+        this.name = name;
+        this.tests = tests;
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + "\nTests: " + tests + "\nPoints: " + points;
+    }
+}
