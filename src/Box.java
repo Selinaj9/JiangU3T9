5e5e91d4203741a41d23737899fa4d7c3c72ec72
BTHS <sample@bths.edu>
2026-02-03 10:56:38 -0500

    Box + extra helpers

---
+++ src/Box.java
@@ -0,0 +1,54 @@
+public class Box {
+    private double length;
+    private double width;
+    private double height;
+
+    public Box(double length, double width, double height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    // creates a cube: a box with all dimensions equal to side
+    public Box(double side) {
+        this(side, side, side);
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double volume() {
+        return length * width * height;
+    }
+
+    public Box greaterVolume(Box otherBox) {
+        if (this.volume() > otherBox.volume()) {
+            return this;
+        } else if (this.volume() < otherBox.volume()) {
+            return otherBox;
+        }
+        return null;
+    }
+
+    public String boxInfo() {
+        return InfoString.boxInfoString(this);
+    }
+
+    public void addToShape(Shape s) {
+        s.attachBox(this);
+    }
+
+    @Override
+    public String toString() {
+        return boxInfo();
+    }
+}
